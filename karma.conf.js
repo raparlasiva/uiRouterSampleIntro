13147cb8108528f15ffe149497a194bdc9a5af9c
Siva Raparla <raparlateja@gmail.com>
2014-12-22 14:51:02 -0500

    install jasmine, and updated angular

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        'bower_components/angular/angular.js',
+       ' bower_components/angular-ui-router/release/angular-ui-router.js'
+        
     ],
 
 
